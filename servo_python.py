4c38bc8824cb006edbd0ec0e9c95fb9d1465e9c4
Mikhail <veltun@gmail.com>
2021-02-26 13:21:46 +0400

    change pwm pin to variable

--- servo_python.py
+++ servo_python.py
@@ -1,14 +1,15 @@
 import RPi.GPIO as GPIO
 from time import sleep
+pwm_pin = 11
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(17, GPIO.OUT)
-pwm=GPIO.PWM(17, 50)
+GPIO.setup(pwm_pin, GPIO.OUT)
+pwm=GPIO.PWM(pwm_pin, 50)
 pwm.start(0)
 def SetAngle(angle):
 	duty = angle / 18 + 2
-	GPIO.output(17, True)
+	GPIO.output(pwm_pin, True)
 	pwm.ChangeDutyCycle(duty)
 	sleep(1)
-	GPIO.output(17, False)
+	GPIO.output(pwm_pin, False)
 	pwm.ChangeDutyCycle(0)
 SetAngle(90)
