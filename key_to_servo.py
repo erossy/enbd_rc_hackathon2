d9cb71f512d03371b1ea2dc65ce90377989e4a4e
Mikhail <veltun@gmail.com>
2021-02-26 15:17:44 +0400

    ddd

--- key_to_servo.py
+++ key_to_servo.py
@@ -61,6 +61,7 @@ def listener_velocity(self, params, packet):
 			print("sending HIGH to dir")
 			#GPIO.output(velocity_dir_gpio_port, 1)
 			print("sending HIGH to speed")
+    print(packet.payload)
 	print("sending LOW to dir")
 
 
